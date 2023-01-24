9ff19f93a2c5fb05fc638dec607ba51f63b8a31e
Andrey <maven23@rambler.ru>
2023-01-24 15:55:49 +0300

    before everything break

---
+++ src/client/pages/main/components/UserItem.vue
@@ -0,0 +1,60 @@
+<template>
+  <div>
+    <div class="user">
+      <div class="userBody">
+        <div>
+          <img class="imgUser" src="../img/logo.png" alt="картинка">
+        </div>
+        <div>
+          <div><strong>Фамилия:</strong> {{ user.surname }}</div>
+          <div><strong>Имя:</strong> {{ user.name }}</div>
+          <div><strong>Отчество</strong> {{ user.patronymic }}</div>
+          <div><strong>Email:</strong> {{ user.email }}</div>
+          <div><strong>Количество друзей:</strong> {{ user.countOfFriends }}</div>
+        </div>
+      </div>
+      <div class="user__btn">
+        <button>Удалить</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    user: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+.user {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 15px;
+  border: 2px solid rosybrown;
+  margin-top: 15px;
+  font-size: 20px;
+  width: 550px;
+  border-radius: 30px;
+}
+
+.userBody {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.imgUser {
+  border: 1px solid #cbc7c7;
+  border-radius: 50%;
+  width: 120px;
+  padding: 5px;
+  margin-right: 30px;
+}
+</style>
--- src/client/pages/main/components/UserList.vue
+++ src/client/pages/main/components/UserList.vue
@@ -1,58 +1,30 @@
 <template>
   <div>
-    <div class="user" v-for="user in users">
-      <div class="userBody">
-        <div>
-          <img class="imgUser" src="../img/logo.png" alt="картинка">
-        </div>
-        <div>
-          <div><strong>Фамилия:</strong> {{ user.surname }}</div>
-          <div><strong>Имя:</strong> {{ user.name }}</div>
-          <div><strong>Отчество</strong> {{ user.patronymic }}</div>
-          <div><strong>Email:</strong> {{ user.email }}</div>
-          <div><strong>Количество друзей:</strong> {{ user.countOfFriends }}</div>
-        </div>
-      </div>
-    </div>
+    <h3>Список пользователей:</h3>
+    <user-item
+        v-for="user in users"
+        :user="user"
+    />
   </div>
 </template>
 
 <script>
+import UserItem from "./UserItem.vue";
+
 export default {
-    props: {
-      users: {
-        type: Array,
-        required: true,
-      }
+  components: {UserItem},
+  props: {
+    users: {
+      type: Array,
+      required: true,
     }
+  }
 }
 </script>
 
 <style scoped>
-.user {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  padding: 15px;
-  border: 2px solid rosybrown;
-  margin-top: 15px;
-  font-size: 20px;
-  width: 550px;
-  border-radius: 30px;
-}
-
-.userBody {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-}
-
-.imgUser {
-  border: 1px solid #cbc7c7;
-  border-radius: 50%;
-  width: 120px;
-  padding: 5px;
-  margin-right: 30px;
+h3 {
+  margin-top: 10px;
 }
 
 </style>
